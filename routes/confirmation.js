1db2287e48b1ba7f9718a7fd80710b6312103a4f
Kai-Yuan Neo <kaiyuan.neo@gmail.com>
2014-04-27 22:20:33 -0700

    just cloned again

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -6,8 +6,8 @@ router.post('/confirmation', function(req, res) {
   var postmark = require("postmark")("dc028761-82f0-471c-a6f9-16fafd8d6571");
   postmark.send({
         "From": "sf.citi <kneo@stanford.edu>", 
-        //"To": "kaiyuan.neo@gmail.com", 
-        "To": "abram@svangel.com", 
+        "To": "kaiyuan.neo@gmail.com", 
+        //"To": "abram@svangel.com", 
         "Subject": "[NEW SIGNUP] New member company", 
         "TextBody": "Test Message"
   });
